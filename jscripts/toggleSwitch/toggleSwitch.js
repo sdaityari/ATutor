b74280c04896ba7cedbddb98eaafca2026417756
Shaumik Daityari <sdaityari@gmail.com>
2013-08-17 22:03:35 +0530

    ui_system: added click functionality to toggle switches

--- jscripts/toggleSwitch/toggleSwitch.js
+++ jscripts/toggleSwitch/toggleSwitch.js
@@ -5,22 +5,44 @@
 // This function would change all radio buttons under selected class into toggle switches
 
 var createToggleSwitch = function (options) {
-    var toggleSwitch = "switch",
+    var toggleSwitchClass = "switch",
         toggleTheme = options.theme || "ios",
         emptySpanClass = "slide-button",
         color = options.color || "yellow",
-        toggleWidth = options.width || "3.6em",
-        toggleHeight = options.height || "1.3em",
         toggleSwitches = $("." + options.className);
 
     $.each(toggleSwitches, function (index, toggleSwitch) {
+        var toggleSwitch = $(toggleSwitch);
+
         $("<span />",{
             "class": emptySpanClass
         }).appendTo(toggleSwitch);
-        $(toggleSwitch).find("label").hide();
+
+        toggleSwitch.click(function () {
+            var inputs = toggleSwitch.find("input"),
+                anchor = inputs[0].checked ? inputs[1] : inputs[0];
+
+            $(anchor).prop("checked", true);
+        });
+
+        toggleSwitch.keyup( function(event) {
+            var inputs = toggleSwitch.find("input"),
+                anchor;
+
+            if (event.keyCode === 37) {
+                anchor = inputs[0];
+            } else if (event.keyCode === 39) {
+                anchor = inputs[1];
+            } else {
+                return;
+            }
+
+            event.preventDefault();
+
+            $(anchor).prop("checked", true);
+        });
     });
 
-    toggleSwitches.attr("style", "width: " + toggleWidth + "; height: " + toggleHeight + ";");
-    toggleSwitches.addClass(toggleSwitch + " " + toggleTheme + " " + color);
+    toggleSwitches.addClass(toggleSwitchClass + " " + toggleTheme + " " + color);
 
-    };
+};
