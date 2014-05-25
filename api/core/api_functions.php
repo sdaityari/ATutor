9b9846e6e0dc48277a4a7112f7cbac726a112045
Shaumik <sdaityari@gmail.com>
2014-05-26 00:17:57 +0530

    API: Condensed code

--- api/core/api_functions.php
+++ api/core/api_functions.php
@@ -7,10 +7,11 @@ if (!defined('AT_INCLUDE_PATH')) {
 function api_module_status(){
     $enabled = queryDB("SELECT * FROM %smodules WHERE dir_name = '%s' and status = %d",
         array(TABLE_PREFIX, "_standard/api", 2));
-    return $enabled;
+    return $enabled[0]?true:false;
 }
 
 function generate_urls($old_array, $prefix) {
+    // Add prefix to all indices of old array
     $new_array = array();
     foreach($old_array as $key => $value) {
         $new_array[$prefix.$key] = $value;
@@ -18,7 +19,9 @@ function generate_urls($old_array, $prefix) {
     return $new_array;
 }
 
-function check_token_exists($token){
+function check_token($token){
+    // TODO - check if token expired as well
+    // TODO - modify modified column in db
     $check = queryDB("SELECT * FROM %sapi WHERE access_token = %s", array(TABLE_PREFIX, $token));
     if ($check[0]) {
         return true;
@@ -29,10 +32,8 @@ function check_token_exists($token){
 }
 
 function get_access_token($headers) {
-    $token = AT_print($headers['x-AT-API-TOKEN']);
-    if (check_token_exists($token)){
-        return $token;
-    }
+    $token = allslashes($headers['x-AT-API-TOKEN']);
+    return check_token($token)?$token:false;
 }
 
 ?>
